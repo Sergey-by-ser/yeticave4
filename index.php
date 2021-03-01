40d64b20f82538cdb9fed8985ce49893d57e7c7a
Sergey-by-ser <Sernarbef@gmail.com>
2021-03-01 13:21:37 +0300

    firsttask

--- index.php
+++ index.php
@@ -28,8 +28,24 @@ $user_name = 'sergey'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
+            <?php if($is_auth):?>
+            <div class="user-menu__image">
+                <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p>$user_name</p>
+            </div>
+            <?php else:?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+            <?php endif;?>
         </nav>
     </div>
 </header>
