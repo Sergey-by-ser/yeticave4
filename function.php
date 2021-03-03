3d80cb623b4ca2064fe5b4d30ee15b4763a0e851
Sergey-by-ser <Sernarbef@gmail.com>
2021-03-03 12:55:47 +0300

    rabota_s_datami

--- function.php
+++ function.php
@@ -77,6 +77,20 @@ function sum_format($number)
 }
 
 
+function timer_to(){
+    $time_now = strtotime('now');
+    $time_tomorrow = strtotime('tomorrow');
+
+    $time_sub = $time_tomorrow-$time_now;
+
+    $time_minutes=floor($time_sub/60);
+    $time_hours=floor($time_minutes/60);
+    $time_minutes=$time_minutes-$time_hours*60;
+
+    $result = sprintf('%02d:%02d',$time_hours,$time_minutes);
+    return $result;
+}
+
 function compile_template($name, $data) {
 
     $name = 'templates/' . $name;
@@ -93,4 +107,6 @@ function compile_template($name, $data) {
     $result = ob_get_clean();
     return $result;
 }
+
+
 ?>
--- templates/index.php
+++ templates/index.php
@@ -31,7 +31,7 @@
                             <span class="lot__cost"><?=sum_format($value['price']);?></span>
                         </div>
                         <div class="lot__timer timer">
-                            12:23
+                            <?=timer_to();?>
                         </div>
                     </div>
                 </div>
